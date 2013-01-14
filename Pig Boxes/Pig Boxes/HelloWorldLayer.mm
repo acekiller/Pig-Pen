3d7ed52aac1ef28b1e71cf410d7ad56d171f1694
Joe Miller <josephclaymiller@gmail.com>
2013-01-13 23:41:37 -0800

    Added pigs xcode project with cocos2D and box2D.

---
+++ Pig Boxes/Pig Boxes/HelloWorldLayer.mm
@@ -0,0 +1,247 @@
+//
+//  HelloWorldLayer.mm
+//  Pig Boxes
+//
+//  Created by Joseph Miller on 1/6/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+// Import the interfaces
+#import "HelloWorldLayer.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+
+#import "PhysicsSprite.h"
+
+enum {
+	kTagParentNode = 1,
+};
+
+
+#pragma mark - HelloWorldLayer
+
+@interface HelloWorldLayer()
+-(void) initPhysics;
+-(void) addNewSpriteAtPosition:(CGPoint)p;
+-(void) createMenu;
+@end
+
+@implementation HelloWorldLayer
+
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	HelloWorldLayer *layer = [HelloWorldLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+	// return the scene
+	return scene;
+}
+
+-(id) init
+{
+	if( (self=[super init])) {
+		
+		// enable events
+		self.isTouchEnabled = YES;
+		//self.isAccelerometerEnabled = YES;
+		CGSize s = [CCDirector sharedDirector].winSize;
+		
+		// init physics
+		[self initPhysics];
+        
+		//Set up sprite
+		
+#if 1
+		// Use batch node. Faster
+		CCSpriteBatchNode *parent = [CCSpriteBatchNode batchNodeWithFile:@"pig_sheet.png" capacity:100];
+		spriteTexture_ = [parent texture];
+#else
+		// doesn't use batch node. Slower
+		spriteTexture_ = [[CCTextureCache sharedTextureCache] addImage:@"pig_sheet.png"];
+		CCNode *parent = [CCNode node];
+#endif
+		[self addChild:parent z:0 tag:kTagParentNode];
+		
+		
+		[self addNewSpriteAtPosition:ccp(s.width/2, s.height/2)];
+		
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
+		[self addChild:label z:0];
+		[label setColor:ccc3(0,0,255)];
+		label.position = ccp( s.width/2, s.height-50);
+		
+        // Initialize array of pigs
+        _pigs = [[NSMutableArray alloc] init];
+        
+        // Add Pigs
+        [self addPigs:20];
+        
+		[self scheduleUpdate];
+	}
+	return self;
+}
+
+-(void) dealloc
+{
+	delete world;
+	world = NULL;
+	
+	[super dealloc];
+}
+
+#pragma mark Game methods
+-(void)addPigs:(int)pigs2add
+{
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    for (int i = 1; i <= pigs2add; i++)
+    {
+        NSLog(@"pig %d", i);
+        //[self addPig];
+        
+        // Determine where to spawn the pig along the Y axis
+        int maxY = winSize.height;
+        int randomY = (arc4random() % maxY);
+        // Determine where to spawn the pig along the X axis
+        int maxX = winSize.width;
+        int randomX = (arc4random() % maxX);
+        
+        [self addNewSpriteAtPosition:ccp(randomX, randomY)];
+    }
+    
+}
+
+-(void) initPhysics
+{
+	
+	CGSize s = [[CCDirector sharedDirector] winSize];
+	
+	b2Vec2 gravity;
+	gravity.Set(0.0f, 0.0f);
+	world = new b2World(gravity);
+	
+	
+	// Do we want to let bodies sleep?
+	world->SetAllowSleeping(true);
+	
+	world->SetContinuousPhysics(true);
+	
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0, 0); // bottom-left corner
+	
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world->CreateBody(&groundBodyDef);
+	
+	// Define the ground box shape.
+	b2EdgeShape groundBox;		
+	
+	// bottom
+	
+	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// top
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// left
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// right
+	groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+}
+
+-(void) addNewSpriteAtPosition:(CGPoint)p
+{
+	CCLOG(@"Add sprite %0.2f x %02.f",p.x,p.y);
+	CCNode *parent = [self getChildByTag:kTagParentNode];
+	
+	//randomly picking one of the images from the sprite sheet 
+	int idx = (CCRANDOM_0_1() > .5 ? 0:1);
+	int idy = (CCRANDOM_0_1() > .5 ? 0:1);
+	PhysicsSprite *sprite = [PhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(36 * idx,57 * idy,36,57)];
+	[parent addChild:sprite];
+	
+	sprite.position = ccp( p.x, p.y);
+	
+	// Define the dynamic body.
+	//Set up a 1m squared box in the physics world
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+    bodyDef.userData = sprite;
+	b2Body *body = world->CreateBody(&bodyDef);
+	
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	//dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
+    dynamicBox.SetAsBox(.5f, .9f);//These are mid points for our pig
+	
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;	
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	body->CreateFixture(&fixtureDef);
+	
+	[sprite setPhysicsBody:body];
+    
+    [_pigs addObject:sprite];
+}
+
+-(void) movePigs: (ccTime) dt
+{
+    NSLog(@"Move %d pigs", _pigs.count);
+    for(b2Body *b = world->GetBodyList(); b; b=b->GetNext()) {
+        if (b->GetUserData() != NULL) {
+            CCSprite *sprite = (CCSprite *)b->GetUserData();
+            sprite.position = ccp(b->GetPosition().x * PTM_RATIO,
+                                  b->GetPosition().y * PTM_RATIO);
+            sprite.rotation += -1 * CC_RADIANS_TO_DEGREES(b->GetAngle());
+        }
+    }
+}
+
+-(void) update: (ccTime) dt
+{
+	//It is recommended that a fixed time step is used with Box2D for stability
+	//of the simulation, however, we are using a variable time step here.
+	//You need to make an informed choice, the following URL is useful
+	//http://gafferongames.com/game-physics/fix-your-timestep/
+	
+	int32 velocityIterations = 8;
+	int32 positionIterations = 1;
+	
+    // Set pig velocities
+    [self movePigs:dt];
+    
+	// Instruct the world to perform a single step of simulation. It is
+	// generally best to keep the time step and iterations fixed.
+	world->Step(dt, velocityIterations, positionIterations);	
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	//Add a new body/atlas sprite at the touched location
+	for( UITouch *touch in touches ) {
+		CGPoint location = [touch locationInView: [touch view]];
+		
+		location = [[CCDirector sharedDirector] convertToGL: location];
+		
+		[self addNewSpriteAtPosition: location];
+	}
+}
+
+
+@end
